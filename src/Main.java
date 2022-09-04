ae23f79e0bbbfa09df2a63f27277a005cd40cfef
E-RAY <reylan@yandex.ru>
2022-09-05 02:08:27 +0800

    задачи №1,2,3 методы

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,49 @@
 public class Main {
-    public static void printSeparator (){
-        System.out.println("++++++++++++++++++");
-        System.out.println("------------------");
-    }
-    public static void printIssues (int issueCount)   {
-        System.out.println(issueCount);
-    }
-    public static int sum (int[] nambers) {
-        int sum = 0;
-        for (int i = 0; i < nambers.length; i++) {
-            sum = sum + nambers[i];
-        }
-        return sum;
-    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int[] issuesByMonth = {4, 6, 7, 9, 2, 5, 12, 3, 7, 10, 6, 7, 1, 8};
-        printSeparator();
-        for (int i = 0; i < issuesByMonth.length; i++) {
-            printIssues(issuesByMonth[i]);
-            if ((i+1)%3 == 0) {
-                printSeparator();
+        //Task-1.
+        printIsLeap(2022);
+        //task-2.
+        printInstallOs(0, 2014);
+        //Task-3.
+        printDeliveryDays(150);
+
+    }
+    // Task-1.
+    private static void printIsLeap ( int year ){
+        if (year%4 == 0 && year%100 != 0 || year%400 == 0) {
+            System.out.println(year + " - высокосный год");
+        }else {
+            System.out.println(year + " - не высокосный год");
+        }
+    }
+    // Task-2.
+    public static void printInstallOs(int osVersion, int productYear) {
+        if (osVersion == 0 && productYear >= 2015) {
+            System.out.println("Установите приложение для iOS по ссылке");
+        } else {
+            if (osVersion == 0 && productYear < 2015) {
+                System.out.println("Установите облегченное приложение для iOS по ссылке");
+            }
+            if (osVersion == 1 && productYear >= 2015) {
+                System.out.println("Установите приложение для Android по ссылке");
+            } else {
+                if (osVersion == 1 && productYear < 2015) {
+                    System.out.println("Установите облегченное приложение для Android по ссылке");
+                }
             }
         }
-        printSeparator();
-        int total = sum(issuesByMonth);
-        printIssues(total);
     }
+    public static void printDeliveryDays(int distance) {
+        int deliveryDays = 1;
+        int interval = 40;
+        int startInterval = 20;
+        if (distance <=20 ) {
+            deliveryDays = 1;
+        }else{
+            deliveryDays = deliveryDays + (int) Math.ceil((distance - startInterval) / (double) interval) ;
+        }
+        System.out.println("Потребуется дней : " + deliveryDays);
+    }
+
 }
