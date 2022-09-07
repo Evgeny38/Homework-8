bca069d7ac01f58247f5b9fe7486f08d003db20e
E-RAY <reylan@yandex.ru>
2022-09-07 09:32:13 +0800

    задачи №1,2,3 методы исправил задачи №2,3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         //Task-1.
         printIsLeap(2022);
         //task-2.
-        printInstallOs(0, 2014);
+        printInstallOs(0, 2015);
         //Task-3.
-        printDeliveryDays(150);
+        System.out.println("Потребуется дней : " + printDeliveryDays(95));
+
 
     }
     // Task-1.
@@ -18,32 +21,34 @@ public class Main {
         }
     }
     // Task-2.
-    public static void printInstallOs(int osVersion, int productYear) {
-        if (osVersion == 0 && productYear >= 2015) {
+    private static void printInstallOs(int osVersion, int productYear) {
+        int currentYear  = LocalDate.now().getYear();
+        if (osVersion == 0 && productYear >= currentYear) {
             System.out.println("Установите приложение для iOS по ссылке");
         } else {
-            if (osVersion == 0 && productYear < 2015) {
+            if (osVersion == 0 && productYear < currentYear) {
                 System.out.println("Установите облегченное приложение для iOS по ссылке");
             }
-            if (osVersion == 1 && productYear >= 2015) {
+            if (osVersion == 1 && productYear >= currentYear) {
                 System.out.println("Установите приложение для Android по ссылке");
             } else {
-                if (osVersion == 1 && productYear < 2015) {
+                if (osVersion == 1 && productYear < currentYear) {
                     System.out.println("Установите облегченное приложение для Android по ссылке");
                 }
             }
         }
     }
-    public static void printDeliveryDays(int distance) {
-        int deliveryDays = 1;
+    // Task-3.
+    private static int printDeliveryDays(int distance) {
+        int printDeliveryDays = 1;
         int interval = 40;
         int startInterval = 20;
         if (distance <=20 ) {
-            deliveryDays = 1;
+            printDeliveryDays = 1;
         }else{
-            deliveryDays = deliveryDays + (int) Math.ceil((distance - startInterval) / (double) interval) ;
+            printDeliveryDays = printDeliveryDays + (int) Math.ceil((distance - startInterval) / (double) interval);
         }
-        System.out.println("Потребуется дней : " + deliveryDays);
+        return printDeliveryDays;
     }
 
 }
